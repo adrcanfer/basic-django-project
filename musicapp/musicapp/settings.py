ca28d93610403a9ae95e5a9ccf831bfb887d42d2
Adrián Cantón Fernández <adriancanton@gmail.com>
2019-01-08 12:06:06 +0100

    Fixed travis failure

--- musicapp/musicapp/settings.py
+++ musicapp/musicapp/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -124,4 +123,9 @@ STATIC_URL = '/static/'
 
 BASE_URL = 'https://musicapp-adrcanfer.herokuapp.com/'
 
-django_heroku.settings(locals())
+try:
+    import django_heroku
+    django_heroku.settings(locals())
+except:
+    None
+
